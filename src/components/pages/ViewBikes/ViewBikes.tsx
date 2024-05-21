c63824f4c769c1234f6d9e7451df825001b7fc93
hammadyounas <m.hammadyounas@outlook.com>
2024-05-21 13:03:53 +0500

    view bikes

--- src/components/pages/ViewBikes/ViewBikes.tsx
+++ src/components/pages/ViewBikes/ViewBikes.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
 
 export default function ViewBikes() {
-  return <div></div>;
+  return (
+    <div className="mx-auto flex w-[95%] flex-col items-center justify-center pt-20 max-lg:flex-col sm:w-[90%] sm:pt-32 lg:w-[70%] xl:w-[50%]">
+      <div className="w-full bg-secondary">
+        <h1 className="text-xl font-bold text-red-600 sm:text-2xl lg:text-3xl">
+          Find Bikes
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam,
+          incidunt.
+        </p>
+      </div>
+    </div>
+  );
 }

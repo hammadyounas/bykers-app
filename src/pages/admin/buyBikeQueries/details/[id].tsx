3bc7a5eeffd2583a9616e112716cc9b4d042006c
Hammad Younus <m.hammadyounas@outlook.com>
2024-07-09 15:05:37 +0500

    integrate query status update api

---
+++ src/pages/admin/buyBikeQueries/details/[id].tsx
@@ -0,0 +1,10 @@
+import QueriesDetails from '@/components/pages/Admin/Queries/QueriesDetails'
+import React from 'react'
+
+export default function BuyBikeQueriesDetails() {
+  return (
+    <div>
+      <QueriesDetails />
+    </div>
+  )
+}
